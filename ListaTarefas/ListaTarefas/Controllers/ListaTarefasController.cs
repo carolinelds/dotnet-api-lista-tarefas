f7d463672e0a0f5dcd2c94bc2a19684a8b4fff7b
Caroline Lopes dos Santos <caroline.santos@iteris.com.br>
2022-06-06 09:21:06 -0300

    Adiciona entidade tarefa

---
+++ ListaTarefas/ListaTarefas/Controllers/ListaTarefasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ListaTarefas.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListaTarefasController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Tarefa> Get()
+        {
+        
+        }
+
+    }
+}
---
+++ ListaTarefas/ListaTarefas/Domain/Entity/Tarefa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Domain.Entity
+{
+    public class Tarefa
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluido { get; set; }
+        public int Prioridade { get; set; }
+    }
+}
