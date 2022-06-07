01baf6d06b6fbff9b9a2920795a07f4a8e41e8f0
Caroline Lopes dos Santos <caroline.santos@iteris.com.br>
2022-06-07 10:33:27 -0300

    Configura arquivo para testes da API

---
+++ ListaTarefas/ListaTarefas.Tests/Services/TarefasServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ListaTarefas.DAL;
+using ListaTarefas.Domain.DTO;
+using ListaTarefas.Services;
+using System;
+using Xunit;
+
+
+namespace ListaTarefas.Tests.Services
+{
+    public partial class TarefasServiceTest : IDisposable
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly TarefasService _service;
+
+        public TarefasServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _dbContext = new AppDbContext(options);
+            _service = new TarefasService(_dbContext);
+        }
+
+
+        public void Dispose()
+        {
+            _dbContext.Database.EnsureDeleted();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
