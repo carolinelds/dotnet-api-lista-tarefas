ff8e64135595ad426529d648ffa162fa0ca418e8
Caroline Lopes dos Santos <caroline.santos@iteris.com.br>
2022-06-07 10:37:50 -0300

    Cria stub de tarefas para testes

--- ListaTarefas/ListaTarefas.Tests/Services/TarefasServiceTest.cs
+++ ListaTarefas/ListaTarefas.Tests/Services/TarefasServiceTest.cs
@@ -2,9 +2,10 @@
 using ListaTarefas.DAL;
 using ListaTarefas.Domain.DTO;
 using ListaTarefas.Services;
+using ListaTarefas.Domain;
 using System;
 using Xunit;
-
+using ListaTarefas.Domain.Entity;
 
 namespace ListaTarefas.Tests.Services
 {
@@ -23,7 +24,34 @@ namespace ListaTarefas.Tests.Services
             _service = new TarefasService(_dbContext);
         }
 
+        private List<Tarefa> ListaTarefasStub()
+        {
+            var lista = new List<Tarefa>()
+            {
+                new Tarefa()
+                {
+                    IdTarefa = 1,
+                    Titulo = "Tarefa Test 1",
+                    Descricao = "Descricao Test 1",
+                    Concluido = false,
+                    Prioridade = 5
+                },
+                new Tarefa()
+                {
+                    IdTarefa = 2,
+                    Titulo = "Tarefa Test 2",
+                    Descricao = "Descricao Test 2",
+                    Concluido = true,
+                    Prioridade = 1
+                }
+            };
 
+            _dbContext.AddRange(lista);
+            _dbContext.SaveChanges();
+
+            return lista;
+        }
+            
         public void Dispose()
         {
             _dbContext.Database.EnsureDeleted();
